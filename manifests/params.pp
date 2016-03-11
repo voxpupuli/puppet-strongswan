230dbcb77bd91fde828bb9a0971c7c8214e58b56
Lukasz Rohde <l.rohde@kainos.com>
2016-03-11 10:57:56 +0100

    remove debian support

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,6 @@
 # Default strongSwan parameters.
 class strongswan::params {
   case $::osfamily {
-    'Debian': {
-      $package                = 'strongswan'
-      $ipsec_d_dir            = '/etc/ipsec.d'
-      $ipsec_conf             = '/etc/ipsec.conf'
-      $ipsec_secrets          = '/etc/ipsec.secrets'
-      $charon_conf            = '/etc/strongswan.d/charon.conf'
-    }
-
     'RedHat': {
       $package                = 'strongswan'
       $ipsec_d_dir            = '/etc/strongswan/ipsec.d'
