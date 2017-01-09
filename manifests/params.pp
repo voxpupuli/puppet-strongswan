20801ed6ea529c656f81bfbb84aa0bd039746b96
Grzegorz Gruszecki <g.gruszecki@kainos.com>
2017-01-09 10:21:16 +0100

    included logging in Debian

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,19 +2,21 @@
 class strongswan::params {
   case $::osfamily {
     'Debian': {
-      $package       = 'strongswan'
-      $ipsec_d_dir   = '/etc/ipsec.d'
-      $ipsec_conf    = '/etc/ipsec.conf'
-      $ipsec_secrets = '/etc/ipsec.secrets'
-      $charon_conf   = '/etc/strongswan.d/charon.conf'
+      $package             = 'strongswan'
+      $ipsec_d_dir         = '/etc/ipsec.d'
+      $ipsec_conf          = '/etc/ipsec.conf'
+      $ipsec_secrets       = '/etc/ipsec.secrets'
+      $charon_conf         = '/etc/strongswan.d/charon.conf'
+      $charon_logging_conf = '/etc/strongswan.d/charon-logging.conf'
     }
 
     'RedHat': {
-      $package       = 'strongswan'
-      $ipsec_d_dir   = '/etc/strongswan/ipsec.d'
-      $ipsec_conf    = '/etc/strongswan/ipsec.conf'
-      $ipsec_secrets = '/etc/strongswan/ipsec.secrets'
-      $charon_conf   = '/etc/strongswan/strongswan.d/charon.conf'
+      $package             = 'strongswan'
+      $ipsec_d_dir         = '/etc/strongswan/ipsec.d'
+      $ipsec_conf          = '/etc/strongswan/ipsec.conf'
+      $ipsec_secrets       = '/etc/strongswan/ipsec.secrets'
+      $charon_conf         = '/etc/strongswan/strongswan.d/charon.conf'
+      $charon_logging_conf = '/etc/strongswan/strongswan.d/charon-logging.conf'
     }
 
     default: {
