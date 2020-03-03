102eae62f245fd35edbdccf0b3c7de470d03cf5a
Aleksei Korotych <lexa@ihpu.org>
2020-03-03 11:27:05 +0100

    strongswan::secrets support selectors

--- spec/defines/secrets_spec.rb
+++ spec/defines/secrets_spec.rb
@@ -24,8 +24,38 @@ describe 'strongswan::secrets', type: :define do
 
       it {
         is_expected.to contain_concat__fragment('ipsec_secrets_secret-user'). \
-          with_content(%r{: ECDSA user\.der})
+          with_content(%r{user : ECDSA user\.der})
       }
+      context 'with empty selectors' do
+        let :params do
+          {
+            selectors: [],
+            options: {
+              'RSA' => 'server.key'
+            }
+          }
+        end
+
+        it {
+          is_expected.to contain_concat__fragment('ipsec_secrets_secret-user'). \
+            with_content(%r{^ : RSA server.key})
+        }
+      end
+      context 'with selectors' do
+        let :params do
+          {
+            selectors: ['my_id', '10.1.2.3'],
+            options: {
+              'ECDSA' => 'user.der'
+            }
+          }
+        end
+
+        it {
+          is_expected.to contain_concat__fragment('ipsec_secrets_secret-user'). \
+            with_content(%r{my_id 10.1.2.3 : ECDSA user\.der})
+        }
+      end
     end
   end
 end
