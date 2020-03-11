72aa889b38309112e7bb6b07b7ea2a1ce213b652
Alexander Fisher <alex@linfratech.co.uk>
2020-03-11 14:35:53 +0000

    Enable acceptance tests and fixup for Debian
    Fixes #37

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,30 +1,18 @@
 require 'beaker-rspec'
+require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
-hosts.each do |host|
-  install_puppet
-  install_package host, 'net-tools'
-  install_package host, 'make'
-  install_package host, 'gcc'
-  install_package host, 'ruby-devel'
-  install_package host, 'epel-release'
-  install_package host, 'openssl'
-  on host, 'yum makecache'
-end
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
-  # Readable test descriptions
   c.formatter = :documentation
 
-  # Configure all nodes in nodeset
   c.before :suite do
-    puppet_module_install(source: proj_root, module_name: 'strongswan')
     hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-concat'), acceptable_exit_codes: [0, 1]
+      install_package(host, 'epel-release') if fact('os.family') == 'RedHat'
     end
   end
 end
