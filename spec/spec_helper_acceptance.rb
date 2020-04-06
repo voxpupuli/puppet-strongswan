6047c839eb7e301da1fa370f01dca990dae15666
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-06 23:12:56 +0200

    Use voxpupuli-acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,18 +1,5 @@
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
-
-RSpec.configure do |c|
-  c.formatter = :documentation
-
-  c.before :suite do
-    hosts.each do |host|
-      install_package(host, 'epel-release') if fact('os.family') == 'RedHat'
-    end
-  end
+configure_beaker do |host|
+  install_package(host, 'epel-release') if fact_on(host, 'os.name') == 'CentOS'
 end
